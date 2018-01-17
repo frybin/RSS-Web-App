81d702b42009351a2f6c9995c0b2835f8972661f
frybin <fredrybin@gmail.com>
2018-01-17 14:16:41 -0500

    Made sure config works

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object('config')
+
 
 from app import routes
--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 from config_keys import *
 
-def Config(object):
-    SECRET_KEY=os.environ.get('SECRET_KEY') or secretkey
+SECRET_KEY=os.environ.get('SECRET_KEY') or secretkey
