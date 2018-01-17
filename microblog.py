ef89380beb9b4884cd7c294fe5cdf4adb015bbcb
frybin <fredrybin@gmail.com>
2018-01-17 16:29:38 -0500

    added database and feed to flask shell

--- microblog.py
+++ microblog.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import Feed
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Feed': Feed}
 
 if __name__ == '__main__':
     app.run()
